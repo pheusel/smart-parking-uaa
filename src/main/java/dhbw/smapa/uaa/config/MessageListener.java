6ccf0f84ece2b01a50f9acfba22f9b3df963d47c
pheusel <philipp.heusel@posteo.de>
2019-06-26 11:55:42 +0200

    message listener constructor added

--- src/main/java/dhbw/smapa/uaa/config/MessageListener.java
+++ src/main/java/dhbw/smapa/uaa/config/MessageListener.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageListener implements Runnable {
 
+    private MqttSubscriber mqttSubscriber;
+
     @Autowired
-    MqttSubscriber mqttSubscriber;
+    public MessageListener(MqttSubscriber mqttSubscriber) {
+        this.mqttSubscriber = mqttSubscriber;
+    }
 
     private static final String SUBSCRIPTION = "/smp/spots";
 
     @Override
     public void run() {
-        while (true) {
-            mqttSubscriber.subscribeMessage(SUBSCRIPTION);
-        }
+        while (true) mqttSubscriber.subscribeMessage(SUBSCRIPTION);
     }
 }
